fe9e4a789c4f85bb0271922eb5d14df632d030ab
Gao Jipeng <xiaogaojia@hotmail.com>
2019-09-09 15:23:27 -0500

    update

--- decoratemytext.js
+++ decoratemytext.js
@@ -25,4 +25,33 @@ function addFontSize() {
 
 function blingChecked() {
     document.body.style.background="url(http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg) no-repeat";
-   }
+}
+
+function translate(str) {
+    if (str.length == 0) return str;
+    var temp = str.replace('\t\t', '');
+    temp = temp.replace(' ', '');
+    if (temp.length == 0) return str;
+    if (/^[a-zA-Z]+$/.test(temp) == false) return str;
+    if (typeof(str) === 'number' && !isNaN(str)) return str;
+    var vowel = ['a', 'e', 'i', 'o', 'u'];
+    if (vowel.indexOf(str.substr(0,1)) !== -1) {
+        return str + 'ay';
+    }
+    while (vowel.indexOf(str.substr(0,1)) == -1) {
+        str = str.substr(1) + str.substr(0,1);
+    }
+    return str + 'ay';
+}
+
+function pigLatin() {
+    var text = document.getElementById('textArea').innerHTML;
+    var textArr = text.split(' ');
+    var finalArr = [];
+    for (let i = 0; i < textArr.length; i++) {
+        var tr = translate(textArr[i]);
+        finalArr.push(tr);
+    }
+    var finalText = finalArr.join(' ');
+    document.getElementById('textArea').innerHTML = finalText;
+}
