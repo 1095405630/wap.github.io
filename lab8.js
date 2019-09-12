2ed410cc2ce8ce2334342c7d1ba487b5780d2bbf
Gao Jipeng <xiaogaojia@hotmail.com>
2019-09-11 21:13:54 -0500

    Add files via upload

--- lab8.js
+++ lab8.js
@@ -56,15 +56,70 @@ const tina = Teacher('Tina');
 tina.teach('WAP');
 
 // 4.
-const Person = {
-    name = 'default',
-    age = '18',
-    greeting : function() {
-        console.log("“Greetings, my name is " + this.name + " and I am " + this.age + " years old.”");
-    },
-    salute : function() {
-        console.log("“Good morning!, and in case I dont see you, good afternoon, good evening and good night!”");
-    }
-};
+// object prototype approach
+// const Person = {
+//     name : 'default',
+//     age : '18',
+//     greeting : function() {
+//         console.log("“Greetings, my name is " + this.name + " and I am " + this.age + " years old.”");
+//     },
+//     salute : function() {
+//         console.log("“Good morning!, and in case I dont see you, good afternoon, good evening and good night!”");
+//     }
+// };
 
-const Student = Object.create(Person);
+// const Student = Object.create(Person);
+// Student.major = 'major';
+// Student.greeting = function() {
+//     console.log("Hey, my name is " + this.name + " and I am studying " + this.major);
+// }
+// const Damon = Object.create(Student);
+// Damon.name = 'Damon';
+// Damon.major = 'WAP';
+// Damon.greeting();
+
+// const Professor = Object.create(Person);
+// Professor.department = 'department';
+// Professor.greeting = function() {
+//     console.log("“Good day, my name is " + this.name + " and I am in the " + this.department + " department.”");
+// }
+// const Tina = Object.create(Professor);
+// Tina.name = "Tina";
+// Tina.department = 'Computer Science';
+// Tina.greeting();
+
+// function constructor
+const Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.greeting = function() {
+    console.log("“Greetings, my name is " + this.name + " and I am " + this.age + " years old.”");
+}
+Person.prototype.salute = function() {
+    console.log("“Good morning!, and in case I dont see you, good afternoon, good evening and good night!”");
+}
+
+const Student = function(name) {
+    Person.call(this, name);
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.major = 'major';
+Student.prototype.greeting = function() {
+    console.log("Hey, my name is " + this.name + " and I am studying " + this.major);
+}
+const Damon = new Student('Damon');
+Damon.major = 'WAP';
+Damon.greeting();
+
+const Professor = function(name) {
+    Person.call(this, name);
+}
+Professor.prototype = Object.create(Person.prototype);
+Professor.prototype.department = 'department';
+Professor.prototype.greeting = function() {
+    console.log("“Good day, my name is " + this.name + " and I am in the " + this.department + " department.”");
+}
+const Tina = new Professor('Tina');
+Tina.department = 'Computer Science';
+Tina.greeting();
